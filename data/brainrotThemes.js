c7d1618082b92b142c508b56ac154f23d4c67ed1
helloprof <suluxan.seneca@gmail.com>
2026-01-23 17:15:16 -0500

    themes js

---
+++ data/brainrotThemes.js
@@ -0,0 +1,19 @@
+let brainrotThemes = [
+    {
+        themeID: "theme-01",
+        theme: "subway surfers"
+    },
+    {
+        themeID: "theme-02",
+        theme: "minecraft"
+
+    },
+    {
+        themeID: "theme-03",
+        theme: "driving simulator"
+    },
+    {
+        themeID: "theme-04",
+        theme: "fortnite"
+    }
+]

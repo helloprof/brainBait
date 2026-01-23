b21a57bcca5454cc7a34ad3a88a8e0cb5e60effd
helloprof <suluxan.seneca@gmail.com>
2026-01-23 17:02:47 -0500

    course data

---
+++ data/courses.js
@@ -0,0 +1,16 @@
+let courses = [
+    {
+        courseID: "BTI225",
+        topics: ["frontend", "html", "css", "javascript"]
+    },
+    {
+        courseID: "BTI325",
+        topics: ["backend", "tailwind", "nodeJS", "sql", "mongodb"]
+    },
+    {
+        courseID: "BTL300",
+        topics: ["probabilities", "distributions", "regression"]
+    }
+]
+
+// courses[0].courseID // returns "BTI225"

1a46c3434bc033a031e9c9145bba83e1d1884a26
helloprof <suluxan.seneca@gmail.com>
2026-03-13 18:03:35 -0400

    add image and color properties to course topics and courses

--- js/courseTopics.js
+++ js/courseTopics.js
@@ -2,23 +2,29 @@ window.courseTopics = [
     {
         courseID: "BTI225",
         topic: "HTML",
+        image: "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         // image: ,
         // description: 
     },
     {
         courseID: "BTI225",
-        topic: "JavaScript"
+        topic: "JavaScript",
+        image: "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
     },
     {
         courseID: "BTI225",
-        topic: "CSS"
+        topic: "CSS",
+        image: "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         courseID: "BTI325",
-        topic: "Tailwind"
+        topic: "Tailwind",
+        image: "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
         courseID: "BTI325",
-        topic: "MongoDB"
+        topic: "MongoDB",
+        image: "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
 ]
--- js/courses.js
+++ js/courses.js
@@ -2,18 +2,22 @@ window.courses = [
     {
         courseID: "BTI225",
         name: "Web Programming Principles",
-        semester: "Winter 2026"
+        semester: "Winter 2026",
+        color: "#FEAB18"
     },
     {
         courseID: "BTI325",
         name: "Web Programming Tools and Frameworks",
-        semester: "Winter 2026"
+        semester: "Winter 2026",
+        color: "#9966FF"
+
     },
 
     {
         courseID: "BTL300",
         name: "Statistics for Developers",
-        semester: "Fall 2025"
+        semester: "Fall 2025",
+        color: "#FF6680"
     }
 ]
 

794422c5199f46e6887c2a3c4875f1b92613f86a
helloprof <suluxan.seneca@gmail.com>
2026-03-13 18:03:26 -0400

    refactor course button generation and improve layout for course topics

--- js/app.js
+++ js/app.js
@@ -7,31 +7,70 @@ console.log({ courses, courseTopics, brainrotThemes }, "App Data");
 
 function generateCourseButtons(courses) {
     // target container/element to append to 
-    let mainDiv = document.querySelector(".start-stop")
+    let coursePanelDiv = document.querySelector(".coursePanel")
 
     // loop through courses array and create buttons 
     courses.forEach((course) => {
 
         // create button for each course
         let button = document.createElement("button")
-        button.textContent = course.courseID
+        // button.textContent = course.courseID
         //  both of these are the same:
         // 1.
         // button.classList.add("courseButton")
 
         // 2.
-        button.className = "courseButton"
+        button.className = "courseButton"        
+        button.style.backgroundColor = course.color
 
-        mainDiv.appendChild(button)
+        let courseLabel = document.createElement("p")
+        courseLabel.className = "courseLabel"
+        courseLabel.textContent = course.courseID
 
-        button.addEventListener("click", () => {
+        let buttonDiv = document.createElement("div")
+        buttonDiv.className = "courseButtonDiv"
+
+        buttonDiv.appendChild(button)
+        buttonDiv.appendChild(courseLabel)
+
+
+        coursePanelDiv.appendChild(buttonDiv)
+        
+        buttonDiv.addEventListener("click", () => {
             console.log(`You clicked on ${course.courseID}`)
-            // next function call happens
+            buttonDiv.style.backgroundColor = "#E9EEF2"
+            generateCourseTopicCards(course.courseID)
         })
     });
 }
 
-function generateCourseCards(courseID) {
+function generateCourseTopicCards(courseID) {
+    let workshop = document.querySelector(".workshop")
+    workshop.innerHTML = ""
+
+    // card creation 
+    let filteredTopics = courseTopics.filter((topic) => topic.courseID === courseID)
+    filteredTopics.forEach((topic) => {
+        let card = document.createElement("div")
+        card.className = "courseCard"
+        
+        let image = document.createElement("img")
+        image.src = topic.image 
+        let title = document.createElement("h2")
+        title.textContent = topic.topic 
+        // let description = document.createElement("p")
+        // description.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate."
+        let button = document.createElement("button")
+        // button.className = "courseButton"
+        button.textContent = topic.courseID
+
+        card.appendChild(image)
+        card.appendChild(title)
+        // card.appendChild(description)
+        card.appendChild(button)
+        
+        workshop.appendChild(card)
+    })
     // card design to be generated:
             // <div class="courseCard">
             //     <img src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
@@ -39,6 +78,7 @@ function generateCourseCards(courseID) {
             //     <p>Creating web pages </p>
             //     <button class="courseButton">BTI225</button>
             //  </div>
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {

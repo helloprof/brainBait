d2f87309d378ed58088fa8cff5a260cb3750148a
helloprof <suluxan.seneca@gmail.com>
2026-04-17 17:40:12 -0400

    add images to themes in brainrotThemes.js

--- js/brainrotThemes.js
+++ js/brainrotThemes.js
@@ -1,19 +1,23 @@
 window.brainrotThemes = [
     {
         themeID: "theme-01",
-        theme: "subway surfers"
+        theme: "subway surfers",
+        image: "https://cdn.iconscout.com/icon/free/png-256/free-subway-surfers-icon-svg-download-png-10673435.png"
     },
     {
         themeID: "theme-02",
-        theme: "minecraft"
+        theme: "minecraft",
+        image: "https://i.redd.it/variations-to-the-minecraft-logo-v0-0dzqovtefeqd1.png?width=1218&format=png&auto=webp&s=4e7beefe76053942332e5c9f4c4f71671f97e63e"
 
     },
     {
         themeID: "theme-03",
-        theme: "driving simulator"
+        theme: "driving simulator",
+        image: "https://play-lh.googleusercontent.com/quFE2rNBamJENQPKPlBGFepStbCGUWguGxYwLUNDEqPi42SupeLMnbPwrWRr0tgsOyJi"
     },
     {
         themeID: "theme-04",
-        theme: "fortnite"
+        theme: "fortnite",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_CFGIYQXkeNwwTZzvd-TYJobk9rf7urZzvg&s"
     }
 ]
